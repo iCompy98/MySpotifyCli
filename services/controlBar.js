048d46bad440194d1f6da3dce5fa7ecf0489f266
iCompy98 <valentin.barreras@gmail.com>
2021-11-25 11:15:50 -0800

    Add active/disactive shuffle

--- services/controlBar.js
+++ services/controlBar.js
@@ -5,16 +5,12 @@ const configFile = './config.json'
 const file = require(configFile)
 
 const changeSong = ( actionEvent ) => {
-				axios.post(`${urlMain}/me/player/${actionEvent}`,null,{
-								headers:{"Authorization": `Bearer ${file.accessToken}`},
-				params:{
-								device_id: file.device
-				}
-				})
-				.then(res=>{
-								console.log("Sip ",res.data)
-				})
-				.catch(err=>console.log("Nop ",err.response.data))
+    axios.post(`${urlMain}/me/player/${actionEvent}`,null,{
+            headers:{"Authorization": `Bearer ${file.accessToken}`},
+		    params:{device_id: file.device}
+        })
+	    .then(res=>console.log("Sip ",res.data))
+		.catch(err=>console.log("Nop ",err.response.data))
 }
 
 const changePlayer = ( actionEvent ) => {
@@ -25,6 +21,16 @@ const changePlayer = ( actionEvent ) => {
        })
     .then(()=>console.log("Hecho!"))
     .catch(err=>console.log("Error ",err.response.data))
+}
+
+const changeShuffle = ( bool ) => {
+    axios.put(`${urlMain}/me/player/shuffle`,null,
+        {
+            headers:{"Authorization": `Bearer ${file.accessToken}`},
+            params:{state: bool,device_id: file.device}
+        })
+    .then(res=>console.log("Hecho!"))
+    .catch(err=>console.log("Ocurrio un error ",err))
 }
 
 switch (process.argv[2]){
@@ -40,18 +46,19 @@ switch (process.argv[2]){
     case "pause":
         changePlayer(process.argv[2])
         break;
+    case "random":
+        if(process.argv[3] === "true"){
+            console.log("Activando random...")
+            changeShuffle(true)
+        }else if(process.argv[3] === "false"){
+            console.log("Desactivando random ...")
+            changeShuffle(false)
+        }else{
+            console.log(`No hay opciones para 'random ${process.argv[3]}'`)
+        }
+        break;
     default:
 	    console.log(`No hay opciones para '${process.argv[2]}'`)
 }
-
-
-
-
-
-
-
-
-
-
 
 
