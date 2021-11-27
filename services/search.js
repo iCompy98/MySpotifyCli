3005d06ce867ffae32bea086c08589a3f933dcaf
iCompy98 <valentin.barreras@gmail.com>
2021-11-26 16:59:08 -0800

    Start the searching
    Now, you can search music by artists

--- services/search.js
+++ services/search.js
@@ -3,8 +3,71 @@ const fs = require('fs');
 const urlMain = "https://api.spotify.com/v1"
 const configFile = './config.json'
 const file = require(configFile)
+const readline = require('readline');
+const CLI = require('clui');
+const clc = require('cli-color');
+const Line = CLI.Line;
 
 //console.log(process.argv)
+
+const artistPrint = (items) => {
+    console.log(items)
+    var header = new Line()
+        .padding(2)
+        .column('Id', 5, [clc.cyan])
+        .padding(2)
+        .column('Nombre', 30, [clc.cyan])
+        .padding(2) 
+        .column('Uri', 40, [clc.cyan])
+        .fill()
+        .output();
+
+    var count=0;
+    for(let art of items){
+        var artLine = new Line ()
+            .padding(2)
+            .column(`${++count}`,5)
+            .padding(2)
+            .column(`${art.name}`,30)
+            .padding(2)
+            .column(`${art.uri}`,40)
+            .fill()
+            .output();
+    }
+    askQuestion("Seleccione mediante su ID: ")
+        .then(res=>{
+            console.log("Seleccionado ", items[res])
+            changePlayer("play",items[res-1].uri)
+        })
+        .catch(err=>console.log(err))
+}
+
+const albumPrint = () => {
+
+}
+
+const trackPrint = () => {
+
+}
+
+const playlistPrint = () => {
+
+}
+
+const changePlayer = ( actionEvent, uri ) => {
+    const body =  {
+        "context_uri": uri,   
+        "position_ms": 0      
+    };
+
+   axios.put(`${urlMain}/me/player/${actionEvent}`,body,
+       {
+           headers:{"Authorization": `Bearer ${file.accessToken}`},
+           params:{device_id: file.device}
+       })
+    .then((res)=>console.log("Hecho! "))
+    .catch(err=>console.log("Error ",err.response))
+}
 
 const getSearch = (type, query) => {
     axios.get(`${urlMain}/search`,
@@ -13,12 +76,30 @@ const getSearch = (type, query) => {
             params:{q:query,type:type}
         })
     .then(res=>{
-        console.log(res.data.albums.items)
-        console.log(res.data.artists.items 
-            ?? res.data.albums.items
-            ?? res.data.playlists.items)
+        let { items } = res.data.artists ?? res.data.albums ?? res.data.tracks
+            ?? res.data.playlists;
+        //console.log(items)
+        let action = {
+            artist: ()=>artistPrint(items),
+            album: ()=>console.log("Metodo albums"),
+            track: ()=>console.log("Metodo track"),
+            playlist: ()=>console.log("Metodo playlist"),
+        }
+        action.artist()
     })
-    .catch(err=>console.log(err.response.data));
+    .catch(err=>console.log(err));
+}
+
+function askQuestion(query) {
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+    });
+
+    return new Promise(resolve => rl.question(query, ans => {
+        rl.close();
+        resolve(ans);
+    }))
 }
 
 getSearch(process.argv[2],process.argv[3]);
