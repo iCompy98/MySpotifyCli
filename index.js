60647fee6b9232cfb79fbb0bde73635db1edd382
iCompy98 <valentin.barreras@gmail.com>
2021-12-08 15:26:19 -0800

    Add config.json

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-#!/usr/bin/env node
+const fs = require('fs')
+
+console.log(process.env.PWD)
+fs.exists(`${process.env.PWD}/client/services/.config.json`,(exist)=>
+    exist ? "Si" : "No"
+)
 
 const getToken = require('./client/services/getToken.js')
 const controlBar = require('./client/controlBar')
@@ -8,9 +13,13 @@ const dashboard = require('./client/dashboard')
 const devices = require('./client/devices')
 const search = require('./client/search')
 
+
 const actions = {
     token: ()=>getToken(),
-    refreshToken: ()=>refreshToken(),
+    refreshToken: ()=>{
+        refreshToken()
+        getToken()
+    },
     controlbar: ()=>controlBar(process.argv[3],process.argv[4]),
     url: ()=>getUrl(),
     dashboard: ()=>dashboard(process.argv[3]),
